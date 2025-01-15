0415e96c726207382bbfd8faf2919702ca02954a
NJittakorn <166985281+NJittakorn@users.noreply.github.com>
2025-01-15 09:37:10 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,15 +9,6 @@
             <td width="43" rowspan="2">&nbsp;</td>
         </tr>
         <tr>
-            <td width="1007" bgcolor="#FFFFFF">
-                <img src="images/map.gif">
-            </td>
-            <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ</li>
-				<li><a href="contactCFK.php">ติดต่อKFC</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
